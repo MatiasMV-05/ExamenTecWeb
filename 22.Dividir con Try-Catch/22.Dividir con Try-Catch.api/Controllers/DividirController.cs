d4979dbf121335009e157bf7a8996322da2d9d3f
MatiasMV-05 <matias.moron@ucb.edu.bo>
2025-09-05 04:29:51 -0400

    Add files via upload

---
+++ 22.Dividir con Try-Catch/22.Dividir con Try-Catch.api/Controllers/DividirController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _22.Dividir_con_Try_Catch.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DividirController : ControllerBase
+    {
+        [HttpGet("{a}/{b}")]
+        public IActionResult Dividir(decimal a, decimal b)
+        {
+            try
+            {
+                decimal resultado = a / b;
+
+                return Ok(resultado);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error.Message);
+            }
+        }
+    }
+}
