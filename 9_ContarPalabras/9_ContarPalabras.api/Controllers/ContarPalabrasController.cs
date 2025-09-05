5209e30b884275a743b9b4c8c2c54f72c248f0a1
MatiasMV-05 <matias.moron@ucb.edu.bo>
2025-09-05 04:21:13 -0400

    Add files via upload

---
+++ 9_ContarPalabras/9_ContarPalabras.api/Controllers/ContarPalabrasController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _9_ContarPalabras.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ContarPalabrasController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult ContarPalabras([FromBody] string texto)
+        {
+            if (string.IsNullOrWhiteSpace(texto))
+            {
+                return BadRequest( "El texto no puede estar vacío" );
+            }
+
+            char[] separadores = new[] { ' ' };
+
+            string[] palabras = texto.Split(separadores, StringSplitOptions.RemoveEmptyEntries);
+
+            return Ok(new
+            {
+                Texto = texto,
+                TotalPalabras = palabras.Length,
+        
+            });
+
+        }
+
+        
+    }
+
+    public class TextoRequest
+    {
+        public string Texto { get; set; }
+    }
+}
