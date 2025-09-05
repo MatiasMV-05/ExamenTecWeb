0132e093a2d65eec72c7b1ca65a1ccc66bfb4bbf
MatiasMV-05 <matias.moron@ucb.edu.bo>
2025-09-05 04:30:40 -0400

    Add files via upload

---
+++ 11_Clase Producto.Api/11_Clase Producto.Api/Controllers/ProductosController.cs
@@ -0,0 +1,42 @@
+﻿using _11_Clase_Producto.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _11_Clase_Producto.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductosController : ControllerBase
+    {
+
+        private static List<Producto> productos =
+            new()
+            {
+                new Producto { Id = 1, Nombre = "Manzana", Precio = 20  },
+                new Producto { Id = 2, Nombre = "Mandarina", Precio = 19  },
+                new Producto { Id = 3, Nombre = "Naranja", Precio = 15  }
+
+            };
+        [HttpGet]
+        public ActionResult<IEnumerable<Producto>> GetAll()
+        {
+            return Ok(productos);
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Producto>
+            GetById(int id)
+        {
+            var producto = productos.FirstOrDefault(x => x.Id == id);
+            if (producto is not null)
+                return Ok(producto);
+            else
+                return NotFound();
+        }
+
+        
+
+    }
+}
+
+
---
+++ 11_Clase Producto.Api/11_Clase Producto.Api/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _11_Clase_Producto.Api.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public float Precio { get; set; }
+   
+
+    }
+}
