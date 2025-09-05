1734c597683fa211a465e6c534607506ed4a4c07
MatiasMV-05 <matias.moron@ucb.edu.bo>
2025-09-05 10:20:10 -0400

    Add files via upload

---
+++ 29.SelectAnónimos/29.SelectAnónimos.api/Controllers/UsuarioController.cs
@@ -0,0 +1,45 @@
+﻿using _29.SelectAnónimos.api.Usuario;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _29.SelectAnónimos.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class UsuarioController : ControllerBase
+    {
+            private static List<Usuarios> _usuarios = new(){
+
+            new Usuarios { Id = 1, Nombre = "Ana", Apellido = "García", Edad = 28 },
+            new Usuarios { Id = 2, Nombre = "Carlos", Apellido = "López", Edad = 35 },
+            new Usuarios { Id = 3, Nombre = "María", Apellido = "Rodríguez", Edad = 22 },
+            new Usuarios { Id = 4, Nombre = "Juan", Apellido = "Martínez", Edad = 42 },
+            new Usuarios { Id = 5, Nombre = "Matias", Apellido = "Moron", Edad = 20 },
+
+            };
+
+            [HttpGet("nombre")]
+            public IActionResult GetUsuariosOrdenados()
+            {
+                try
+                {
+                    var nombres = _usuarios.Select(u => new { u.Nombre }).ToList();
+
+                    if (!nombres.Any())
+                    {
+                        return NotFound("No se encontraron usuarios" );
+                    }
+
+                    return Ok(nombres);
+                }
+                catch (Exception error)
+            {
+                    return BadRequest(error.Message);
+                }
+
+
+            }
+        
+    }
+}
---
+++ 29.SelectAnónimos/29.SelectAnónimos.api/Models/Usuarios.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _29.SelectAnónimos.api.Usuario
+{
+    public class Usuarios
+    {
+      
+            [Key]
+            public int Id { get; set; }
+            [Required]
+            public string Nombre { get; set; }
+
+            [Required]
+            public string Apellido { get; set; }
+
+            [Required]
+            public int Edad { get; set; }
+        
+    }
+}
