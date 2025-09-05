2a7073be1beb9d98330f191224154e7b669d9caa
MatiasMV-05 <matias.moron@ucb.edu.bo>
2025-09-05 04:09:23 -0400

    Add files via upload

---
+++ 8.Diccionario_Traducciones.api/8.Diccionario_Traducciones.api/Controllers/TraducirController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _8.Diccionario_Traducciones.api.Controllers
+{
+        [Route("api/[controller]")]
+        [ApiController]
+        public class TraducirController : ControllerBase
+        {
+            private static Dictionary<string, string> _traducciones = new()
+        {
+            {"hello", "hola"},
+            {"goodbye", "adiós"},
+            {"glass", "vaso"},
+            {"please", "por favor"},
+            {"yes", "sí"},
+            {"school", "colegio"},
+            {"water", "agua"},
+            {"food", "comida"},
+            {"house", "casa"},
+            {"dog", "perro"},
+            {"cat", "gato"},
+            {"love", "amor"},
+            {"friend", "amigo"},
+            {"time", "tiempo"},
+            {"work", "trabajo"}
+        };
+
+            [HttpGet("{palabra}")]
+            public ActionResult<object> Traducir(string palabra)
+            {
+         
+
+                if (_traducciones.TryGetValue(palabra, out string? traduccion))
+                {
+                    return Ok(new
+                    {
+                        PalabraIngles = palabra,
+                        PalabraEspanol = traduccion,
+                    });
+                }
+                else
+                {
+                    return NotFound(new
+                    {
+                        PalabraIngles = palabra,
+                        Mensaje = "Palabra no encontrada en el diccionario"
+                    });
+                }
+            }
+
+            [HttpGet("todas")]
+            public ActionResult<object> ObtenerTodasLasTraducciones()
+            {
+                return Ok(_traducciones);
+            }
+
+           
+        }
+}
+
+
---
+++ 8.Diccionario_Traducciones.api/8.Diccionario_Traducciones.api/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace _8.Diccionario_Traducciones.api
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
