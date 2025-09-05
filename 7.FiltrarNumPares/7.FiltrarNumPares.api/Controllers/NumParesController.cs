d666297927573201074a4158399bb21832bc4141
MatiasMV-05 <matias.moron@ucb.edu.bo>
2025-09-05 10:38:34 -0400

    Add files via upload

---
+++ 7.FiltrarNumPares/7.FiltrarNumPares.api/Controllers/NumParesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FiltersNumParser.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NumParesController : ControllerBase
+    {
+        [HttpPost("filtrar-pares")]
+        public ActionResult<object> FiltrarPares([FromBody] List<int> numeros)
+        {
+            if (numeros == null || !numeros.Any())
+            {
+                return BadRequest("La lista de números no puede estar vacía");
+            }
+
+            List<int> numerosPares = new List<int>();
+
+            foreach (int numero in numeros)
+            {
+                if (numero % 2 == 0)
+                {
+                   numerosPares.Add(numero) ;
+                }
+            }
+
+            return Ok(new
+            {
+                TotalPares = numerosPares.Count,
+                NumerosPares = numerosPares,
+            });
+        }
+    }
+}
