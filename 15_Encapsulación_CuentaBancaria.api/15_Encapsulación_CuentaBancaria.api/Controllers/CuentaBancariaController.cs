38f16de6c9b934f9ea7867851df8e47c8933854a
MatiasMV-05 <matias.moron@ucb.edu.bo>
2025-09-05 04:24:06 -0400

    Add files via upload

---
+++ 15_Encapsulación_CuentaBancaria.api/15_Encapsulación_CuentaBancaria.api/Controllers/CuentaBancariaController.cs
@@ -0,0 +1,91 @@
+﻿using _15_Encapsulación_CuentaBancaria.api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _15_Encapsulación_CuentaBancaria.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CuentaBancariaController : ControllerBase
+    {
+        private static List<CuentaBancaria> cuentas = new()
+        {
+            new CuentaBancaria
+            {
+                NumeroCuenta = 1234,
+                nombre = "Juan Perez",  
+              
+            },
+            new CuentaBancaria
+            {
+                NumeroCuenta = 5678,
+                nombre = "Maria Gomez",  
+            },
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CuentaBancaria>> GetAll()
+        {
+            return Ok(cuentas);
+        }
+
+        [HttpGet("{numeroCuenta:int}")]
+        public ActionResult<CuentaBancaria> GetByNumeroCuenta(int numeroCuenta)
+        {
+            var cuenta = cuentas.FirstOrDefault(x => x.NumeroCuenta == numeroCuenta);
+            if (cuenta is not null)
+                return Ok(cuenta);
+            else
+                return NotFound();
+        }
+
+        [HttpPost("depositar/{NumeroCuenta}/{Monto}")]
+        public ActionResult<CuentaBancaria> Depositar(int NumeroCuenta, decimal Monto) 
+        {
+            try
+            {
+                var cuenta = cuentas.FirstOrDefault(x => x.NumeroCuenta == NumeroCuenta);
+                if (cuenta is null)
+                    return NotFound("Cuenta no encontrada");
+
+                cuenta.Depositar(Monto);  
+
+                return Ok(new
+                {
+                    Mensaje = "Depósito realizado exitosamente",
+                    NumeroCuenta = NumeroCuenta,
+                    NuevoSaldo = cuenta.GetSaldo()
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("retirar/{NumeroCuenta}/{Monto}")]
+        public ActionResult<CuentaBancaria> Retirar(int NumeroCuenta, decimal Monto)  
+        {
+            try
+            {
+                var cuenta = cuentas.FirstOrDefault(x => x.NumeroCuenta == NumeroCuenta);
+                if (cuenta is null)
+                    return NotFound("Cuenta no encontrada");
+
+                cuenta.Retirar(Monto);  
+
+                return Ok(new
+                {
+                    Mensaje = "Retiro realizado exitosamente",
+                    NumeroCuenta = NumeroCuenta,
+                    NuevoSaldo = cuenta.GetSaldo()  
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+
+   
+}
---
+++ 15_Encapsulación_CuentaBancaria.api/15_Encapsulación_CuentaBancaria.api/Models/CuentaBancaria.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _15_Encapsulación_CuentaBancaria.api.Models
+{
+    public class CuentaBancaria
+    {
+        [Key]
+        public int NumeroCuenta { get; set; }
+
+        [Required]
+        public string nombre { get; set; }
+
+        private decimal Saldo;
+
+        public decimal SaldoAux
+        {
+            get { return Saldo; }
+            private set { Saldo = value; }
+        }
+
+        public void Depositar(decimal monto)
+        {
+            if (monto <= 0)
+                throw new Exception("El monto a depositar debe ser mayor a cero.");
+            Saldo += monto;
+        }
+
+        public void Retirar(decimal monto)
+        {
+            if (monto <= 0)
+                throw new Exception("El monto a retirar debe ser mayor a cero.");
+            if (monto > Saldo)
+                throw new Exception("Fondos insuficientes para realizar el retiro.");
+            Saldo -= monto;
+        }
+
+        public decimal GetSaldo()
+        {
+            return Saldo;
+        }
+    }
+}
+
